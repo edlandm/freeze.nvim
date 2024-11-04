5c3d35ad73e9725551b3212f41542cf6b5fb9478
Miles Edland <miles.edland@gmail.com>
2024-11-04 09:58:16 -0800

    add freeze/types.lua

---
+++ lua/freeze/types.lua
@@ -0,0 +1,7 @@
+--- @alias jumpDest 'start' | 'end' | 'top' | 'bottom' | 'origin'
+--- @alias operatorOpts { jump:jumpDest? }
+--- @alias operatorPositions { top:integer, bottom:integer, start:integer, end:integer }
+--- @alias visualOpts { jump:'top'|'bottom'|'origin'?, resume_visual:boolean? }
+--- @alias position [integer, integer]
+--- @alias selectionType 'char' | 'line' | 'block
+--- @alias visualRange { top:position, bottom:position, selection_type:selectionType }
